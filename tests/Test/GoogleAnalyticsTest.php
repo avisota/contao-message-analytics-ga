6f74438befd6837a7c0e892651b2417f6ce9ed25
Sven Baumann <baumann.sv@gmail.com>
2016-01-23 15:33:59 +0100

    make travis happy add clean phpunit test

---
+++ tests/Test/GoogleAnalyticsTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright © 2016 Sven Baumann
+ *
+ * PHP version 5
+ *
+ * @copyright  way.vision 2016
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @package    avisota/contao-core
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+namespace Avisota\Test\Contao\Message\Analytics\GA;
+
+/**
+ * Class GoogleAnalyticsTest
+ *
+ * @package Avisota\Test\Contao\Message\Analytics\GA
+ */
+class GoogleAnalyticsTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testPrepareButton()
+    {
+    }
+}
